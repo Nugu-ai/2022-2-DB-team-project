5ce1d6f2dd8275ee8d1c80fca0557ed9e6d15f57
jmy0819 <jmy0819_@naver.com>
2022-11-26 21:41:41 +0900

    table select

--- routes/dblogin.py
+++ routes/dblogin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import Blueprint, render_template, request, session, redirect, url_for, current_app
 import pymysql
 import pandas as pd
 import db
@@ -15,8 +15,6 @@ def dblogin():
     1) DB 연결 X:
         - GET: dblogin.html 표시
         - POST: 입력된 정보로 DB 접속 시도, 성공 시 session에 DB 정보 저장
-    2) DB 연결 O: 
-        - CSV 업로드 창으로 redirect ('/dblogin/upload')
     """
     if 'database' not in session:
         if request.method == 'POST':
@@ -29,13 +27,24 @@ def dblogin():
                 conn.close()
                 for key in DB:
                     session[key] = DB[key]
-                return redirect(url_for('dblogin.upload'))
-            except db.DBConnectionError:
-                return render_template("dblogin.html", err='DB 정보 오류')
+                return render_template(
+                        'dblogin.html',
+                        host=session['host'],
+                        port=session['port'],
+                        database=session['database'],
+                        user=session['user'],
+                        msg='DB 연결 성공')
+            except pymysql.Error:
+                return render_template("dblogin.html", msg='DB 연결 실패')
         else:
             return render_template("dblogin.html")
     else:
-        return redirect(url_for('dblogin.upload'))
+        return render_template(
+                'dblogin.html',
+                host=session['host'],
+                port=session['port'],
+                database=session['database'],
+                user=session['user'])
 
 
 # ===================================================
@@ -57,42 +66,68 @@ def upload():
             file = request.files['csv']
             df = pd.read_csv(file)
             
-            # TABLE 생성
-            stmt = ''
-            for col in df.columns:
-                if df[col].dtype == int:
-                    stmt += f'`{col}` INT, '
-                elif df[col].dtype == float:
-                    stmt += f'`{col}` DOUBLE, '
-                else:
-                    stmt += f'`{col}` VARCHAR(100), '
-            create_stmt = f'CREATE TABLE IF NOT EXISTS `{file.filename.split(".")[0]}` ({stmt[:-2]})'
-            
             conn = db.get_db()
             with conn:
                 cur = conn.cursor()
-                cur.execute(create_stmt)
 
-                # Insert row by row
-                for idx in df.index:
-                    stmt = ''
+                # CSV -> TABLE 생성
+                table_name = file.filename.split(".")[0]
+                all_table_list_stmt = f'SELECT `table_name` FROM `TABLE_LIST`'
+                cur.execute(all_table_list_stmt)
+                all_table_list = []
+                for names in cur.fetchall():
+                    all_table_list.append(names[0])
+
+                if table_name not in all_table_list:
+                    col_stmt = ''
                     for col in df.columns:
-                        if df[col].dtype == object:
-                            stmt += f"'{df[col][idx]}', "
+                        if df[col].dtype == int:
+                            col_stmt += f'`{col}` INT, '
+                        elif df[col].dtype == float:
+                            col_stmt += f'`{col}` DOUBLE, '
                         else:
-                            stmt += f"{df[col][idx]}, "
-                    insert_stmt = f'INSERT INTO `{file.filename.split(".")[0]}` VALUES ({stmt[:-2]})'
-                    print(insert_stmt)
-                    cur.execute(insert_stmt)
+                            col_stmt += f'`{col}` VARCHAR(100), '
+                    create_stmt = f'CREATE TABLE IF NOT EXISTS `{table_name}` ({col_stmt[:-2]})'
+                    cur.execute(create_stmt)
 
-                conn.commit()
+                    for idx in df.index:
+                        stmt = ''
+                        for col in df.columns:
+                            if df[col].dtype == object:
+                                stmt += f"'{df[col][idx]}', "
+                            else:
+                                stmt += f"{df[col][idx]}, "
+                        insert_stmt = f'INSERT INTO {table_name} VALUES ({stmt[:-2]})'
+                        cur.execute(insert_stmt)
+
+                    # TABLE_LIST, ATTR 테이블에 추가
+                    table_list_stmt = f'INSERT INTO `TABLE_LIST` VALUES ("{table_name}", "F")'
+                    count_stmt = f'SELECT COUNT(*) FROM {table_name}'
+                    cur.execute(count_stmt)
+                    record_count = cur.fetchone()[0]
+                    attr_stmt = 'INSERT INTO `ATTR`(table_name, attr_name, data_type, record_count) VALUES '
+                    for col in df.columns:
+                        if df[col].dtype == int:
+                            attr_stmt += f'("{table_name}", "{col}", "INT", {record_count}), '
+                        elif df[col].dtype == float:
+                            attr_stmt += f'("{table_name}", "{col}", "DOUBLE", {record_count}), '
+                        else:
+                            attr_stmt += f'("{table_name}", "{col}", "VARCHAR", {record_count}), '
+                    cur.execute(table_list_stmt)
+                    cur.execute(attr_stmt[:-2])
+
+                    conn.commit()
+                    msg = f'{table_name} 테이블 생성 성공'
+
+                else:
+                    msg = f'동일한 이름의 테이블이 존재합니다: {table_name}'
 
             return render_template(
                 'upload.html',
                 host=session['host'],
                 port=session['port'],
                 database=session['database'],
-                msg=f'{file.filename} 업로드 성공')
+                msg=msg)
         else:
             return render_template(
                 'upload.html',
@@ -101,3 +136,21 @@ def upload():
                 database=session['database'])
     else:
         return redirect(url_for('dblogin.dblogin'))
+
+
+# ===================================================
+# DB 연결 해제
+# ===================================================
+@bp.route('/disconnect', methods=['POST'])
+def disconnect():
+    """
+    session에 저장된 DB 정보를 삭제
+    """
+    if 'database' in session:
+        session.pop('host', None)
+        session.pop('port', None)
+        session.pop('database', None)
+        session.pop('user', None)
+        session.pop('password', None)
+
+    return redirect(url_for('dblogin.dblogin'))
--- routes/tablescan.py
+++ routes/tablescan.py
@@ -1,8 +1,49 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+import db
 
 
 bp = Blueprint('tablescan', __name__, url_prefix='/tablescan', template_folder='templates')
 
-@bp.route('/')
-def dblogin():
-    return render_template("tablescan.html")
+
+# ===================================================
+# 대상 테이블 선택
+# ===================================================
+@bp.route('/', methods=['GET'])
+def tablescan():
+    try:
+        conn = db.get_db()
+        with conn:
+            cur = conn.cursor()
+            table_list_stmt = 'SELECT `table_name` FROM TABLE_LIST'
+            cur.execute(table_list_stmt)
+            all_table_list = []
+            for table_name in cur.fetchall():
+                attr_list_stmt = f'SELECT `attr_name`, `record_count` FROM `ATTR` WHERE `table_name` = "{table_name[0]}"'
+                cur.execute(attr_list_stmt)
+                attrs = []
+                records = 0
+                for attr_name, record_count in cur.fetchall():
+                    attrs.append(attr_name)
+                    records = record_count
+                all_table_list.append({"table_name": table_name[0], "attrs": attrs, "records": records})
+        return render_template(
+                'tablescan.html',
+                host=session['host'],
+                port=session['port'],
+                database=session['database'],
+                result=all_table_list)
+
+    except db.DBConnectionError:
+        return render_template(
+                'tablescan.html',
+                host=session['host'],
+                port=session['port'],
+                database=session['database'])
+
+
+# ===================================================
+# 테이블 속성 도메인 스캔
+# ===================================================
+@bp.route('/<table_name>')
+def scan_table(table_name):
+    pass
