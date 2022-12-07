71e10a3be11a45d4f07d970452483e6193d8b119
2021147598 <gomi0904@yonsei.ac.kr>
2022-12-08 03:02:13 +0900

    singlejoin_target

--- routes/result.py
+++ routes/result.py
@@ -1,8 +1,65 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session, redirect, url_for, jsonify
+import pymysql
+import db
 
 
 bp = Blueprint('result', __name__, url_prefix='/result', template_folder='templates')
 
+
+
 @bp.route('/')
-def dblogin():
-    return render_template("result.html")
+def select():
+    global result_root 
+    result_root = url_for('result.select')
+    
+    return render_template("result.html",result_root = result_root)
+
+
+
+@bp.route("/scan/")
+def scan():
+    global result_root
+    
+    conn = db.get_db()
+    cursor = conn.cursor()
+    
+    numerical_row_sql = """
+        SELECT ATTR.table_name, ATTR.attr_name, data_type, record_count, distinct_count, null_count, null_count/record_count,  zero_count, zero_count/record_count,  min_value, max_value
+        From ATTR, NUMERIC_ATTR
+        WHERE ATTR.table_name = NUMERIC_ATTR.table_name 
+        AND ATTR.attr_name = NUMERIC_ATTR.attr_name
+    """
+    
+    categorical_row_sql = """
+        SELECT ATTR.table_name, ATTR.attr_name, data_type, record_count, distinct_count, null_count,  null_count/record_count,  symbol_count
+        From ATTR, CATEGORICAL_ATTR
+        WHERE ATTR.table_name = CATEGORICAL_ATTR.table_name 
+        AND ATTR.attr_name = CATEGORICAL_ATTR.attr_name
+    """
+    
+    cursor.execute(numerical_row_sql)
+    numerical_rows = cursor.fetchall()
+    
+    cursor.execute(categorical_row_sql)
+    categorical_rows = cursor.fetchall()
+    
+    return render_template("result.html",result_root = result_root,
+                           type = 'scan', 
+                           numeric_rows = numerical_rows,
+                           categoric_rows = categorical_rows)
+
+@bp.route("/single/")
+def single():
+    global result_root
+        
+    return render_template("result.html",
+                           type = 'single',
+                           result_root = result_root)
+
+@bp.route("/multiple/")
+def multiple():
+    global result_root
+        
+    return render_template("result.html",
+                           type = 'multiple',
+                           result_root = result_root)
--- routes/singlejoin.py
+++ routes/singlejoin.py
@@ -20,6 +20,59 @@ def source_table_search():
 @bp.route('/<table_name>', methods=['GET', 'POST'])
 def target_table_search(table_name):
     tabledname = table_name
+    conn = db.get_db()
+    with conn:
+        cur = conn.cursor()
 
-    return render_template("singlejoin_target.html")
+        cur.execute('SELECT record_count FROM attr WHERE table_name = %s', [tabledname])
+        total_record = cur.fetchall()[0][0]
+
+        # 대표속성
+        reprattr = []
+
+        cur.execute('SELECT repr_attr_name FROM repr_attr, std_repr_attr WHERE repr_attr.repr_attr_id = std_repr_attr.repr_attr_id AND table_name = %s', [tabledname])
+        for repr_attr_name in cur.fetchall():
+            reprattr.append(repr_attr_name)
+
+        # 대표결합키
+        joinkey = []
+
+        cur.execute('SELECT key_name FROM join_key, std_join_key WHERE join_key.join_key_id = std_join_key.key_id AND table_name = %s', [tabledname])
+        for key_name in cur.fetchall():
+            joinkey.append(key_name)
+
+        fin_reprattr = ''
+        fin_joinkey = ''
+
+        if len(reprattr) == 0:
+            fin_reprattr = '-'
+        else:
+            reprattr = set(reprattr)
+            if len(list(reprattr)) == 1:
+                for i in range(0, len(list(reprattr))):
+                    fin_reprattr = fin_reprattr + str(list(reprattr)[i][0])    
+            else:
+                for i in range(0, len(list(reprattr)) - 1):
+                    fin_reprattr = fin_reprattr + str(list(reprattr)[i][0]) + ', '
+                fin_reprattr += str(list(reprattr)[len(list(reprattr))][0])
+
+        if len(joinkey) == 0:
+            fin_joinkey = '-'
+        else:
+            joinkey = set(joinkey)
+            if len(list(joinkey)) == 1:
+                for i in range(0, len(list(joinkey))):
+                    fin_joinkey = fin_joinkey + str(list(joinkey)[i][0])
+            else:
+                for i in range(0, len(list(joinkey)) - 1):
+                    fin_joinkey = fin_joinkey + str(list(joinkey)[i][0]) + ', '
+                fin_joinkey += str(list(joinkey)[len(list(joinkey)) - 1][0])
+    
+    return render_template(
+        'singlejoin_target.html',
+        table_name = tabledname,
+        total_record = total_record,
+        repr_attr = fin_reprattr,
+        join_key = fin_joinkey
+    )
 
