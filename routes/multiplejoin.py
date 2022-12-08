ed3a1711b5963a911fe95f1b6ff759849d09b769
2021147598 <gomi0904@yonsei.ac.kr>
2022-12-08 17:52:09 +0900

    multiplejoin search final

--- routes/multiplejoin.py
+++ routes/multiplejoin.py
@@ -1,8 +1,66 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for, request
+import db
 
 
 bp = Blueprint('multiplejoin', __name__, url_prefix='/multiplejoin', template_folder='templates')
 
-@bp.route('/')
-def multiplejoin():
-    return render_template("multiplejoin_source.html")
+# ===================================================
+# source 테이블 검색
+# ===================================================
+@bp.route('/', methods=['GET'])
+def source_table_search():
+    if 'database' in session:
+        return render_template("multiplejoin_source.html")
+    else:
+        return redirect(url_for('dblogin.dblogin'))
+
+# ===================================================
+# target 테이블 검색
+# ===================================================
+@bp.route('/<source_table_name_jk>', methods=['GET', 'POST'])
+def target_table_search(source_table_name_jk):
+    tabledname = source_table_name_jk.split('+')[0]
+    joinkey = source_table_name_jk.split('+')[1]
+
+    conn = db.get_db()
+    with conn:
+        cur = conn.cursor()
+
+        cur.execute('SELECT record_count FROM attr WHERE table_name = %s', [tabledname])
+        total_record = cur.fetchall()[0][0]
+
+        # 대표속성
+        reprattr = []
+
+        cur.execute('SELECT repr_attr_name FROM repr_attr, std_repr_attr WHERE repr_attr.repr_attr_id = std_repr_attr.repr_attr_id AND table_name = %s', [tabledname])
+        for repr_attr_name in cur.fetchall():
+            reprattr.append(repr_attr_name)
+
+        fin_reprattr = ''
+
+        if len(reprattr) == 0:
+            fin_reprattr = '-'
+        else:
+            reprattr = set(reprattr)
+            if len(list(reprattr)) == 1:
+                for i in range(0, len(list(reprattr))):
+                    fin_reprattr = fin_reprattr + str(list(reprattr)[i][0])    
+            else:
+                for i in range(0, len(list(reprattr)) - 1):
+                    fin_reprattr = fin_reprattr + str(list(reprattr)[i][0]) + ', '
+                fin_reprattr += str(list(reprattr)[len(list(reprattr))][0])
+
+    
+    return render_template(
+        'multiplejoin_target.html',
+        table_name = tabledname,
+        total_record = total_record,
+        repr_attr = fin_reprattr,
+        join_key = joinkey
+    )
+# ===================================================
+# 단일 결합 결과
+# ===================================================
+@bp.route('/<source_table_name_jk>/<target_tables>', methods=['GET', 'POST'])
+def single_result(source_table_name_jk, target_tables):
+    return render_template('multiplejoin_result.html')
