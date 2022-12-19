52e10b91725d5dae21eb63d91901699dd3113243
2021147598 <gomi0904@yonsei.ac.kr>
2022-12-19 11:49:28 +0900

    commit for pull

--- routes/result.py
+++ routes/result.py
@@ -108,16 +108,17 @@ def scan_result(table_name):
         
         #이 아랫부분 수정 필요할수도 있음 - sql 반환값이 없을 경우 어떻게 되는지 모름
         cursor.execute(represent_attr_sql)
+        
         if cursor.fetchone() is None :
             val = "x"
-        else :
+        else:
             val = cursor.fetchone()[0]
         numerical_rows[i].append(val)
         
         cursor.execute(join_key_sql)
         if cursor.fetchone() is None :
             val = "x"
-        else :
+        else:
             val = cursor.fetchone()[0]
         numerical_rows[i].append(val)
     
@@ -142,15 +143,16 @@ def scan_result(table_name):
         cursor.execute(represent_attr_sql)
         if cursor.fetchone() is None :
             val = "x"
-        else :
+        else:
             val = cursor.fetchone()[0]
         categorical_rows[i].append(val)
         
         cursor.execute(join_key_sql)
         if cursor.fetchone() is None :
             val = "x"
-        else :
+        else:
             val = cursor.fetchone()[0]
+
         categorical_rows[i].append(val)
         
     return render_template("result.html", result_root = result_root,
